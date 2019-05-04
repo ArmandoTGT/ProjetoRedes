2ddc007fe216830215cefcd564a8483113460b39
Drayton80 <drayton80@hotmail.com>
2019-05-04 13:21:27 -0300

    Beta Documentação e Revisão de TClient

--- Tipo-TwitterServer/src/server/TClient.java
+++ Tipo-TwitterServer/src/server/TClient.java
@@ -22,28 +22,49 @@ import java.util.logging.Logger;
 import javax.swing.JTextField;
 
 
+/**
+* CLASSE - TClient (Thread of Client):
+*   Representa um Cliente em execução (sua Thread), tendo também as funções 
+*   de leitura e escrita dos dados
+*
+*   @author  Drayton Corrêa Filho
+*   @author  Armando de Souza Salvador Neto
+*   @since   29-04-2019 
+*/
 public class TClient implements Runnable {
-    //Cria o hash para salvar os seguidores usando o IP como chave
+    //Cria o hash para salvar os seguidores usando o IP como chave primária
     HashMap<String, String[]> seguidores = new HashMap<String, String[]>();
-    //Cria um semaforo para organizar o acesso a leitura e escria do "banco de dados"
+    //Cria um semáforo para organizar o acesso a leitura e escria do "banco de dados"
     Semaphore sem = new Semaphore(1);
     //Cria as strings com os nomes dos arquivos usados como banco de dados e LOG
     String seguidoresFile = "Seguidores.txt";
     String menssagensFile = "Menssagens.txt";
     String logFile = "Log.txt";
 
-
     Socket cliente;
     DataInputStream entrada;
     DataOutputStream saida;
 
-    //Construtor que recebe o socket
+    /**
+    * Construtor:
+    *   Recebe o Socket do cliente e atualiza os seguidores
+    *   conhecidos no HashMap
+    * 
+    *   @param cliente É o Socket relativo ao cliente
+    */
     public TClient(Socket cliente) throws IOException{
         this.cliente = cliente;
         //Carrega os seguidores conhecidos no hash
         seguidores = attSeguidores(seguidores);
     }
 
+
+    /**
+    * Método - run:
+    *   Override do método run, padrão de qualquer thread. Ele
+    *   serve como fluxo principal de execução de uma thread e
+    *   é o responsável por dar inicio ao paralelismo da mesma 
+    */
     @Override
     public void run(){
         try {
@@ -51,49 +72,54 @@ public class TClient implements Runnable {
             entrada = new DataInputStream(cliente.getInputStream());
             saida = new DataOutputStream(cliente.getOutputStream());
             
-            //Checa se o IP conctado é de algum seguidor ou um ip novo
+            //Checa se o IP conectado é de algum seguidor ou um IP novo
             if(seguidores.get(cliente.getInetAddress().getHostAddress()) == null){
-               //Se for novo ele vai enviar 0 e receber o nick dele, depois ele salva no hash junto com o ip e porta dele
-               sem.acquire();
+               //Se for novo ele vai enviar 0 e receber o nick 
+               //dele, depois ele salva no hash junto com o ip e porta dele
+               sem.acquire();   //Usa o semáforo para apenas um thread acessar por vez
                saida.writeUTF("0");
                String[] entradas = new String[2];
                entradas[0] = String.valueOf(cliente.getPort());
                entradas[1] = entrada.readUTF();
-               seguidores.put(cliente.getInetAddress().getHostAddress(),
-                        entradas);
-               sem.release();
+               seguidores.put(cliente.getInetAddress().getHostAddress(), entradas);
+               sem.release();   //Libera o semáforo para voltar a ser paralelo
             }else{
-               //Se o IP for de um seguidor ele envia o nick para ele e atualiza a porta que ele se conectou
-               sem.acquire();
+               //Se o IP for de um seguidor ele envia 
+               //o nick para ele e atualiza a porta que ele se conectou
+               sem.acquire();   //Usa o semáforo para apenas um thread acessar por vez
                saida.writeUTF(seguidores.get(cliente.getInetAddress().getHostAddress())[1]);
                seguidores.get(cliente.getInetAddress().getHostAddress())[0] = String.valueOf(cliente.getPort());
-               sem.release();
-            }            
-            sem.acquire();
+               sem.release();   //Libera o semáforo para voltar a ser paralelo
+            }    
+
+            sem.acquire();   //Usa o semáforo para apenas um thread acessar por vez
             //Escreve o novo seguidor no arquivo de seguidores
             writeInFile(seguidoresFile, formatHashToWrite(seguidores));
             //Pega as menssagens que ja foram publicadas que mostra pro novo cliente
             String str = String.join("\n\n", readFile(menssagensFile)); 
             saida.writeUTF(str);
-            sem.release();
-            
+            sem.release();   //Libera o semáforo para voltar a ser paralelo
+
         } catch (IOException ex) {
             Logger.getLogger(TClient.class.getName()).log(Level.SEVERE, null, ex);
         } catch (InterruptedException ex) {
             Logger.getLogger(TClient.class.getName()).log(Level.SEVERE, null, ex);
         }
+
         //Esse while continua até o cliente deslogar
         while (cliente.isConnected()) {
             String menssagem;
             try {
-                //Recebe a menssagem enviada
+                //Recebe a mensagem enviada
                 menssagem = entrada.readUTF();
                 sem.acquire();
-                //Pegamos a data e hora que a menssagem foi enviada
+
+                //Pegamos a data e hora que a mensagem foi enviada
                 DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy, HH:mm:ss");
                 java.util.Date d = new Date();
                 String dStr = dateFormat.format(d);
-                //Checamos para ver se é uma menssagem para parar de seguir o servidor
+
+                //Checamos para ver se é uma mensagem para parar de seguir o servidor
                 if(menssagem.equals("Console:UNFOLLOW:1B71")){
                     //Se for ele tira o seguidor do hash de seguidores, retira ele do arquivo e depois desconecta o cliente
                     seguidores.remove(cliente.getInetAddress().getHostAddress());
@@ -101,12 +127,13 @@ public class TClient implements Runnable {
                     writeInFile(seguidoresFile, formatHashToWrite(seguidores));
                     cliente.close();             
                 }
-                //Checamos para ver se é uma menssagem mandar atualizar
+
+                //Checamos para ver se é uma mensagem que manda atualizar
                 if(menssagem.equals("Console:ATTALLMENSSAGE:1B71")){
-                    //Enviamos as menssagens atualizadas
+                    // Se for, enviamos as mensagens atualizadas
                     saida.writeUTF(String.join("\n\n", readFile(menssagensFile)));
                 }else{
-                    //Adicionamos o remetente e a Data/hora a menssagem
+                    //Caso contrário, adicionamos o remetente e a Data/hora a mensagem
                     String nMenssagem = "@" + seguidores.get(cliente.getInetAddress().getHostAddress())[1] + ": " +
                             menssagem + "(" + "Enviado em: " + dStr + ")";
                     //Adicionamos ela ao arquivo de menssagens
@@ -122,105 +149,164 @@ public class TClient implements Runnable {
             } catch (InterruptedException ex) {
                 Logger.getLogger(TClient.class.getName()).log(Level.SEVERE, null, ex);
             }
-
         }
     }
     
-    //Função que tranforma o hash de reguidores em um array de String para poder escrever ele no arquivo
+
+    /**
+    * Método - formatHashToWrite (Format Hash to Write in File):
+    *   Transforma o HashMap de seguidores em um array de Strings para
+    *   possibilitar a escrita dele em um arquivo
+    * 
+    *   @param hash É o HashMap que será transformado em um array
+    *
+    *   @return String[] É um array em que cada elemento será uma linha do arquivo contendo a informação do Hash 
+    */
     public String[] formatHashToWrite(HashMap<String, String[]> hash){
         String[] lines = new String[hash.size()];
         int i = 0;
+
+        //Para cada chave primária no hash
         for ( String key : hash.keySet() ) {
-                String[] resto = hash.get(key);                
-                lines[i] = key + " " + resto[0] + " " + resto[1];                
-                i++;
+            //usa ela para pegar o que está escrito em formato de String
+            String[] resto = hash.get(key);                
+            //e transformar em um array o qual cada elemento será
+            //uma linha do arquivo final aonde será salvo o Hash 
+            lines[i] = key + " " + resto[0] + " " + resto[1];                
+            
+            i++;
         }
         
         return lines;
     }
     
-    //Função que escreve um Array de string em um arquivo, onde cara posisão do array é u m linha de txt
-    public static void writeInFile(String filename, String[] x) throws IOException{       
-        
-        
+
+    /**
+    * Método - writeInFile:
+    *   Escreve um Array de Strings num arquivo txt, aonde cada linha
+    *   conterá a informação de cada elemento do HashMap
+    * 
+    *   @param filename É o nome do arquivo aonde sera salvo os dados
+    *   @param x Array cujos dados serão salvos no arquivo
+    */
+    public static void writeInFile(String filename, String[] x) throws IOException{               
         try (BufferedWriter outputWriter = new BufferedWriter(new FileWriter(filename))) {
-            
             for(int i = 0; i < x.length; i++){  
                 outputWriter.write(x[i]);
                 outputWriter.newLine();
             }
+
             outputWriter.flush();    
         }
 
     }
     
-    //Função que ler arquivo txt e salva cada linha em um posição do array
+
+    /**
+    * Método - readFile:
+    *   Lê um arquivo txt e salva cada linha dele como um elemento de um Array
+    * 
+    *   @param filename É o nome do arquivo aonde sera salvo os dados
+    *   @return String[] O Array aonde as linhas do arquivo serão salvas
+    */
     public String[] readFile(String filename) throws FileNotFoundException, IOException{
-    
-    try(BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-        boolean continua = true;
-        List<String> listaLines = new ArrayList<String>();
-        StringBuilder text = new StringBuilder();
-        String readStringLine = reader.readLine();
-        listaLines.add(readStringLine);       
+        try(BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            boolean continua = true;
+            List<String> listaLines = new ArrayList<String>();
+            StringBuilder text = new StringBuilder();
+            String readStringLine = reader.readLine();
+            listaLines.add(readStringLine);       
 
 
-        while(continua){
-            
-            readStringLine = reader.readLine();  
-            if(readStringLine != null){
-            
-            listaLines.add(readStringLine);
-            }else{
-                continua = false;
+            while(continua){
+                readStringLine = reader.readLine();
+
+                if(readStringLine != null){
+                    listaLines.add(readStringLine);
+                }else{
+                    continua = false;
+                }
             }
+            
+            String[] lines = listaLines.toArray(new String[listaLines.size()]);
+
+            return  lines;
         }
-        
-        String[] lines = listaLines.toArray(new String[listaLines.size()]);
-        return  lines;
-    }
     
     }
-    //Função que ler arquivo txt e salva cada linha em um posição do array, mas essa é feita para ja retornar com a ultima menssagem recebida
+
+
+    /**
+    * Método - readFile:
+    *   Lê um arquivo txt e salva cada linha dele como um elemento de um Array e, além disso,
+    *   retorna ele com a última mensagem recebida já inclusa
+    * 
+    *   @param filename É o nome do arquivo aonde sera salvo os dados
+    *   @param menssagem Última mensagem recebida que será adicionada ao array
+    *
+    *   @return String[] O Array aonde as linhas do arquivo serão salvas em conjunto com a última mensagem recebida
+    */
     public String[] readFile(String filename, String menssagem) throws FileNotFoundException, IOException{
     
-    try(BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-        boolean continua = true;
-        List<String> listaLines = new ArrayList<String>();
-        StringBuilder text = new StringBuilder();
-        String readStringLine = reader.readLine();
-        listaLines.add(readStringLine);       
+        try(BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            boolean continua = true;
+            List<String> listaLines = new ArrayList<String>();
+            StringBuilder text = new StringBuilder();
+            String readStringLine = reader.readLine();
+            listaLines.add(readStringLine);       
 
+            while(continua){
+                readStringLine = reader.readLine();
 
-        while(continua){
-            
-            readStringLine = reader.readLine();  
-            if(readStringLine != null){
-            
-            listaLines.add(readStringLine);
-            }else{
-                continua = false;
+                if(readStringLine != null){
+                    listaLines.add(readStringLine);
+                }else{
+                    continua = false;
+                }
             }
+
+            listaLines.add(menssagem);
+            
+            String[] lines = listaLines.toArray(new String[listaLines.size()]);
+            return  lines;
         }
-        listaLines.add(menssagem);
-        
-        String[] lines = listaLines.toArray(new String[listaLines.size()]);
-        return  lines;
-    }
     
     }
     
+
+    /**
+    * Método - attLog:
+    *   Atualiza o Log de mensagens salvas no arquivo
+    * 
+    *   @param menssagem Última mensagem recebida que será adicionada ao Log
+    *   @param dataHora Data e hora em que a mensagem foi enviada
+    *   @param seguidor Contém o IP e nome de usuário do Seguidor para salvar no Log 
+    *
+    *   @exception IOException Em caso de erro ao tentar acessar o arquivo
+    *   @see IOException
+    */
     public void attLog(String menssagem, String dataHora, String[] seguidor) throws IOException{
         String ip = cliente.getInetAddress().getHostAddress();
         String newLog = "<" + seguidor[1] + ", " + ip +  ":" + seguidor[0] + ", "
                 + dataHora + ", " + menssagem + ">";
         
         writeInFile(logFile, readFile(logFile, newLog));
-    
     }
 
 
-    //Essa função pega os seguidores ja conhecidos do nosso "banco de dados" e coloca em um hash
+    /**
+    * Método - attSeguidores:
+    *   Lê os Seguidores salvos no arquivo Seguidores.txt e os retorna em uma
+    *   HashMap que contém eles
+    * 
+    *   @param hash HashMap aonde serão atualizados os Seguidores
+    *
+    *   @exception FileNotFoundException Em caso de erro ao tentar buscar o arquivo
+    *   @exception IOException Em caso de erro ao tentar acessar o arquivo
+    *   @see IOException
+    *
+    *   @return HashMap atualizado com os Seguidores
+    */
     public static HashMap<String, String[]> attSeguidores(HashMap<String, String[]> hash)
             throws FileNotFoundException, IOException {
         //Abre o arquivo no modo reader
@@ -232,14 +318,12 @@ public class TClient implements Runnable {
             //Salva cada linha em uma lista de string criada a cima
             while(continua){
             
-            readStringLine = reader.readLine();  
-            if(readStringLine != null){
-            
-            
-            lines.add(readStringLine);
-            }else{
-                continua = false;
-            }  
+                readStringLine = reader.readLine();  
+                if(readStringLine != null){
+                    lines.add(readStringLine);
+                }else{
+                    continua = false;
+                }  
             }
 
             String[] linesArray = lines.toArray(new String[lines.size()]);
@@ -251,12 +335,12 @@ public class TClient implements Runnable {
                 String[] entradas = new String[2];
                 entradas[0] = partes[1];
                 entradas[1] = partes[2];
+
                 hash.put(partes[0], entradas);
                 
             }
 
             return hash;
         }
-
     }
 }
